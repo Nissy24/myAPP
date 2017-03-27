a3f9c5e3d375d29042b40a8a4f87059df69f3422
Nissy <nishisuji.0509@gmail.com>
2017-03-27 15:18:10 +0800

    Initial Commit

---
+++ nishisuji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nishisuji
+//
+//  Created by 西筋啓人 on 2017/03/27.
+//  Copyright © 2017年 Hiroto Nishisuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
