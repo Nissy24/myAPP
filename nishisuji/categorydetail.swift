2fec6fb394a4dcd77985aabc340145c1a93df2c5
Nissy <nishisuji.0509@gmail.com>
2017-03-30 14:17:08 +0800

    ⑤：保存ボタンと戻るボタン設置、日付の機能

--- nishisuji/categorydetail.swift
+++ nishisuji/categorydetail.swift
@@ -1,37 +1,20 @@
 //
-//  newFashionViewController.swift
+//  categorydetail.swift
 //  nishisuji
 //
-//  Created by 西筋啓人 on 2017/03/29.
+//  Created by 西筋啓人 on 2017/03/30.
 //  Copyright © 2017年 Hiroto Nishisuji. All rights reserved.
 //
 
 import UIKit
 
-class newFashionViewController: UIViewController {
+class categorydetail: UIViewController {
 
-    @IBOutlet weak var fashion: UIImageView!
-    
-    @IBOutlet weak var mydetail: UITextView!
-    
-    @IBOutlet weak var mydate: UILabel!
-    
-    @IBOutlet weak var mymemo: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        let df = DateFormatter()
-        df.dateFormat = "yyyy/MM/dd"
-        
-        print(df.string(from: Date()))
 
-        mydate.text = df.string(from: Date())
-        
         // Do any additional setup after loading the view.
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
